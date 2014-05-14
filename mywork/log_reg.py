73b01fa2b8d06417b0682e14778d81f776e42964
Roy Xue <xljroy@gmail.com>
2014-05-14 15:35:18 +0800

    update log_reg

--- mywork/log_reg.py
+++ mywork/log_reg.py
@@ -3,13 +3,19 @@
 #reference: Theano:
 #			Cousera Machine Learning by Andrew Ng
 
-import numpy 
-import Theano
-import Theano.tensor as T 
+import numpy as np
+import theano
+import theano.tensor as T 
 
 
 class Logistic_Regression(object):
 
-	def __init__(self):
+	def __init__(self, input):
+		self.data = np.loadtxt(input, delimiter=',')
+		self.x = self.data[:, (0, 1)]
+		self.y = self.data[:, 2]
+		print self.x[1]
 
+
+log_reg = Logistic_Regression("ex2data1.txt")
 		
